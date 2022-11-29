cb9f216a93f36cc25a3aec94244473ec83600f56
Marcialas <martynaspalsys@gmail.com>
2022-11-29 11:52:34 +0000

    Add global variables and start making blueprint of functions that will need for game to function

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+#global variables
+GRID = 5
+USER_BOARD =[]
+COMP_BOARD = []
+USER_GUESS_BOARD = []
+COMP_GUESS_BOARD = []
+TRANSLATE_LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+def print_board():
+    pass
+
+def 
+
