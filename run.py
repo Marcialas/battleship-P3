f4f5d179a9d90757fc8a2502a645c4a790b2e9ad
Marcialas <martynaspalsys@gmail.com>
2022-11-29 12:05:42 +0000

    previous commit, but forgot to save

--- run.py
+++ run.py
@@ -9,11 +9,24 @@ COMP_GUESS_BOARD = []
 TRANSLATE_LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board():
+    '''print board function'''
     pass
 
 
 def place_ships():
+    '''place ships on the board'''
     pass
+
+
+def user_input():
+    '''get user input'''
+    pass
+
+
+def ships_hit():
+    '''count ships hit'''
+    pass
+
 
 
 
