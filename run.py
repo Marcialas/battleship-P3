909d124403f729abe1eeab98557a0a1ab842bda5
Marcialas <martynaspalsys@gmail.com>
2022-12-14 10:46:34 +0000

    Finnaly code a working solution for handling empty user input for program not to crash

--- run.py
+++ run.py
@@ -17,7 +17,6 @@ TRANSLATE_LETTERS_TO_NUMBERS = {
     }
 
 
-
 def print_board(board):
     '''print board function'''
     print('  A B C D E F G H ')
@@ -38,61 +37,17 @@ def place_ships(board):
         board[ship_row][ship_col] = 'O'
 
 
-# def user_input():
-#     '''get users shot location input'''
-#     row = input('Please enter your shot row (1-8): ')
-#     while row not in '12345678':
-#         print('Please enter a valid row (1-8) ')
-#         row = input('Please enter your shot row (1-8): ')
-#     col = input('Please enter your shot column (A-H): ').upper()
-#     while col not in 'ABCDEFGH':
-#         print('Please enter a valid column (A-H')
-#         col = input('Please enter your shot column (A-H): ').upper()
-#     return int(row) - 1, TRANSLATE_LETTERS_TO_NUMBERS[col]
-
-
-
-
-# def user_input():
-#     '''get users shot location input'''
-#     while True:
-#         try:
-#             row = input('Please enter your shot row (1-8): ')
-#             while row not in '12345678':
-#                 print('enter valid row')
-#                 row = input('Please enter your shot row (1-8): ')
-#                 continue
-#             col = input('Please enter your shot column (A-H): ').upper()
-#             while col not in 'ABCDEFGH':
-#                 print('Please enter a valid column (A-H')
-#                 col = input('Please enter your shot column (A-H): ').upper()
-#                 continue
-#         except ValueError:
-#             print('error1')
-#     return int(row) - 1, TRANSLATE_LETTERS_TO_NUMBERS[col]
-
-
 def user_input():
-    while True:
-        try:
-            row = input('Please enter your shot row (1-8): ')
-            if row not in '12345678':
-                print('enter valid row')
-                continue
-            col = input('Please enter your shot column (A-H): ').upper()
-            if col not in 'ABCDEFGH':
-                print('enter valid letter')
-                continue
-            else:
-                break
-            return int(row) - 1, TRANSLATE_LETTERS_TO_NUMBERS[col]
-        except TypeError:
-            print('enter valid letter')
-            continue
-
-
-        
-
+    '''get users shot location input'''
+    row = input('Please enter your shot row (1-8): ')
+    while row not in '12345678' or row == '':
+        print('Please enter a valid row (1-8) ')
+        row = input('Please enter your shot row (1-8): ')
+    col = input('Please enter your shot column (A-H): ').upper()
+    while col not in 'ABCDEFGH' or col == '':
+        print('Please enter a valid column (A-H')
+        col = input('Please enter your shot column (A-H): ').upper()
+    return int(row) - 1, TRANSLATE_LETTERS_TO_NUMBERS[col]
 
 
 def ships_hit(board):
